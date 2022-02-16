0ee43d3e3bbd75dc3e0f946ef442c42b3c28fe2f
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-16 17:08:26 +0100

    Added operations function to debug results

---
+++ operations.js
@@ -0,0 +1,27 @@
+const chalk = require("chalk");
+const debug = require("debug")("calculator:index");
+
+const operations = (a, b) => {
+  debug(
+    chalk.bgBlack.blue(
+      `${chalk.yellow(a)} + ${chalk.magenta(b)} = ${chalk.green(a + b)}`
+    )
+  );
+  debug(
+    chalk.bgBlack.blue(
+      `${chalk.yellow(a)} - ${chalk.magenta(b)} = ${chalk.green(a - b)}`
+    )
+  );
+  debug(
+    chalk.bgBlack.blue(
+      `${chalk.yellow(a)} * ${chalk.magenta(b)} = ${chalk.green(a * b)}`
+    )
+  );
+  debug(
+    chalk.bgBlack.blue(
+      `${chalk.yellow(a)} / ${chalk.magenta(b)} = ${chalk.green(a / b)}`
+    )
+  );
+};
+
+module.exports = operations;
