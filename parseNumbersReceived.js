ed8810eb1b5743e1d10afaf69764a2fae194f78a
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-16 17:13:54 +0100

    Added parseInt function to the received numbers

---
+++ parseNumbersReceived.js
@@ -0,0 +1,6 @@
+const receivedNumbersString = require("./userInputs");
+
+const aNumber = parseInt(receivedNumbersString.aNumber);
+const bNumber = parseInt(receivedNumbersString.bNumber);
+
+module.exports = { aNumber, bNumber };
